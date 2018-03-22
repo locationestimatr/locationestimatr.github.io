7de33278aff01015b5c1a881c611bf3eec7b2b63
Ruurd Bijlsma <ruurdbijlsma@gmail.com>
2018-03-23 00:39:01 +0100

    improved high score view

--- highscore/script.js
+++ highscore/script.js
@@ -10,6 +10,26 @@ rules = {
     }
 };
 
+async function setBackground(map) {
+    let response = await fetch('../data/maps.json');
+    let maps = await response.json();
+    if (map in maps) {
+        document.querySelector('.background').style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('../data/thumbnails/${map}.jpg')`;
+    } else {
+        response = await fetch('../data/flag-names.json');
+        let flagNames = await response.json();
+        let flagCode = flagNames[map];
+
+        console.log(flagNames, map, flagCode);
+
+        document.querySelector('.background').style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('../data/thumbnails/flags/${flagCode.toLowerCase()}.svg')`;
+    }
+}
+
+function nicify(name) {
+    return name.split('_').map(word => word[0].toUpperCase() + word.slice(1)).join(' ');
+}
+
 async function init() {
     let map = decodeURI(location.hash.substring(1));
     window.addEventListener('hashchange', () => {
@@ -18,12 +38,18 @@ async function init() {
     if (map === '')
         map = 'world';
 
+    let niceMap = nicify(map);
+
+    document.querySelector('.map-name').innerText = niceMap;
+    document.querySelector('.play-map').href = '../play/#' + map;
+
     scores = new Scores();
+    setBackground(map);
 
     let localScoreElement = document.querySelector('.local-high-score');
     let localScores = scores.getLocalHighScores(map, rules.default);
     if (localScores.length === 0) {
-        localScoreElement.innerText = `You don't have any scores on "${map}" yet`;
+        localScoreElement.innerText = `You don't have any scores on "${niceMap}" yet`;
     } else {
         displayScores(localScoreElement, localScores);
     }
@@ -31,7 +57,7 @@ async function init() {
     let globalScoreElement = document.querySelector('.global-high-score');
     let globalHighScores = await scores.getGlobalHighScores(map, rules.default);
     if (globalHighScores.length === 0) {
-        globalScoreElement.innerText = `There are no scores on "${map}" yet`;
+        globalScoreElement.innerText = `There are no scores on "${niceMap}" yet`;
     } else {
         displayScores(globalScoreElement, globalHighScores);
     }
--- homepage/script.js
+++ homepage/script.js
@@ -1,28 +1,34 @@
-document.addEventListener('DOMContentLoaded',init);
+document.addEventListener('DOMContentLoaded', init);
 
-async function init(){
+async function init() {
     let response = await fetch('./data/countries.json');
     let countries = await response.json();
 
     response = await fetch('./data/flag-names.json');
     let flagNames = await response.json();
-    
+
     response = await fetch('./data/streetview-countries.json');
     let streetviewCountries = await response.json();
 
     console.log(streetviewCountries);
 
     let html = '';
-    for(let country in countries){
+    for (let country in countries) {
         let flagCode = flagNames[country];
-        if(!flagCode || !streetviewCountries.includes(country))
+        if (!flagCode || !streetviewCountries.includes(country))
             continue;
 
-        html +=`
-        <a class="map" href=./play#${encodeURI(country)}>
-            <div class="map-background" style="background-image:url(data/thumbnails/flags/${flagCode.toLowerCase()}.svg)"></div>
-            <div class="map-title">${country}</div>
-        </a>
+        html += `
+            <div class="map">
+                <div class="map-background" style="background-image:url(data/thumbnails/flags/${flagCode.toLowerCase()}.svg)"></div>
+                <div class="map-text">
+                    <div class="map-title">${country}</div>
+                    <div class="map-actions">
+                        <a href=./play#${encodeURI(country)} class="map-play">Play</a>
+                        <a href=./highscore#${encodeURI(country)} class="map-score">Scores</a>
+                    </div>
+                </div>
+            </div>
         `;
     }
     document.querySelector('.country-maps').innerHTML = html;
